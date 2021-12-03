fb1baec3bf691b8556217be137708aba47fa226a
Luca Borzacchiello <lucaborza@gmail.com>
2021-12-03 20:08:34 +0100

    fix run.sh

--- apks/run.sh
+++ apks/run.sh
@@ -6,6 +6,16 @@ function static_mapping {
     echo ""
     echo "Using FlowDroid:"
     liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./StaticMapping.apk 2> /dev/null
+    echo ""
+}
+
+function static_mapping_overload {
+    echo "Using Androguard:"
+    liana --reachable --find-path-to libnative.so@target_fun ./StaticMappingOverload.apk 2> /dev/null
+    echo ""
+    echo "Using FlowDroid:"
+    liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./StaticMappingOverload.apk 2> /dev/null
+    echo ""
 }
 
 if [ "$1" != "" ]; then
@@ -14,3 +24,4 @@ if [ "$1" != "" ]; then
 fi
 
 static_mapping
+static_mapping_overload
