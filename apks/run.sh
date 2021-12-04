ca58bec4c34548a3b983a5f1846c5aad7cd961df
Luca Borzacchiello <lucaborza@gmail.com>
2021-12-04 11:54:01 +0100

    run.sh fixes

--- apks/run.sh
+++ apks/run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 function static_mapping {
+    echo "static_mapping"
+    echo "=============="
     echo "Using Androguard:"
     liana --reachable --find-path-to libnative.so@target_fun ./StaticMapping.apk 2> /dev/null
+    echo ""
+    echo "Full iCFG Path:"
+    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./StaticMapping.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
     liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./StaticMapping.apk 2> /dev/null
@@ -10,8 +15,13 @@ function static_mapping {
 }
 
 function static_mapping_overload {
+    echo "static_mapping_overload"
+    echo "======================="
     echo "Using Androguard:"
     liana --reachable --find-path-to libnative.so@target_fun ./StaticMappingOverload.apk 2> /dev/null
+    echo ""
+    echo "Full iCFG Path:"
+    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./StaticMappingOverload.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
     liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./StaticMappingOverload.apk 2> /dev/null
