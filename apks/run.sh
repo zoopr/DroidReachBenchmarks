40803339d47abbb85ff3ade702009860732210dd
Luca Borzacchiello <lucaborza@gmail.com>
2022-08-27 12:54:57 +0200

    fix run.sh: change tool name

--- apks/run.sh
+++ apks/run.sh
@@ -4,13 +4,13 @@ function static_mapping {
     echo "static_mapping"
     echo "=============="
     echo "Using Androguard:"
-    liana --reachable --find-path-to libnative.so@target_fun ./StaticMapping.apk 2> /dev/null
+    dreach --reachable --find-path-to libnative.so@target_fun ./StaticMapping.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./StaticMapping.apk 2> /dev/null
+    dreach --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./StaticMapping.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./StaticMapping.apk 2> /dev/null
+    dreach --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./StaticMapping.apk 2> /dev/null
     echo ""
 }
 
@@ -18,13 +18,13 @@ function static_mapping_overload {
     echo "static_mapping_overload"
     echo "======================="
     echo "Using Androguard:"
-    liana --reachable --find-path-to libnative.so@target_fun ./StaticMappingOverload.apk 2> /dev/null
+    dreach --reachable --find-path-to libnative.so@target_fun ./StaticMappingOverload.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./StaticMappingOverload.apk 2> /dev/null
+    dreach --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./StaticMappingOverload.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./StaticMappingOverload.apk 2> /dev/null
+    dreach --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./StaticMappingOverload.apk 2> /dev/null
     echo ""
 }
 
@@ -32,13 +32,13 @@ function dynamic_mapping {
     echo "dynamic_mapping"
     echo "==============="
     echo "Using Androguard:"
-    liana --reachable --find-path-to libnative.so@target_fun ./DynamicMapping.apk 2> /dev/null
+    dreach --reachable --find-path-to libnative.so@target_fun ./DynamicMapping.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./DynamicMapping.apk 2> /dev/null
+    dreach --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./DynamicMapping.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./DynamicMapping.apk 2> /dev/null
+    dreach --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./DynamicMapping.apk 2> /dev/null
     echo ""
 }
 
@@ -46,13 +46,13 @@ function dynamic_mapping_complex {
     echo "dynamic_mapping_complex"
     echo "======================="
     echo "Using Androguard:"
-    liana --reachable --find-path-to libnative.so@target_fun ./DynamicMappingComplex.apk 2> /dev/null
+    dreach --reachable --find-path-to libnative.so@target_fun ./DynamicMappingComplex.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./DynamicMappingComplex.apk 2> /dev/null
+    dreach --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./DynamicMappingComplex.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./DynamicMappingComplex.apk 2> /dev/null
+    dreach --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./DynamicMappingComplex.apk 2> /dev/null
     echo ""
 }
 
@@ -60,13 +60,13 @@ function dynamic_mapping_clash {
     echo "dynamic_mapping_clash"
     echo "====================="
     echo "Using Androguard:"
-    liana --reachable --find-path-to libnative.so@target_fun ./DynamicMappingClash.apk 2> /dev/null
+    dreach --reachable --find-path-to libnative.so@target_fun ./DynamicMappingClash.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./DynamicMappingClash.apk 2> /dev/null
+    dreach --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./DynamicMappingClash.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./DynamicMappingClash.apk 2> /dev/null
+    dreach --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./DynamicMappingClash.apk 2> /dev/null
     echo ""
 }
 
@@ -74,13 +74,13 @@ function dynamic_mapping_nonglobal {
     echo "dynamic_mapping_nonglobal"
     echo "========================="
     echo "Using Androguard:"
-    liana --reachable --find-path-to libnative.so@target_fun ./DynamicMappingNonglobal.apk 2> /dev/null
+    dreach --reachable --find-path-to libnative.so@target_fun ./DynamicMappingNonglobal.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./DynamicMappingNonglobal.apk 2> /dev/null
+    dreach --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./DynamicMappingNonglobal.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./DynamicMappingNonglobal.apk 2> /dev/null
+    dreach --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./DynamicMappingNonglobal.apk 2> /dev/null
     echo ""
 }
 
@@ -88,13 +88,13 @@ function nested_lib {
     echo "nested_lib"
     echo "=========="
     echo "Using Androguard:"
-    liana --reachable --find-path-to libnested.so@target_fun ./NestedLib.apk 2> /dev/null
+    dreach --reachable --find-path-to libnested.so@target_fun ./NestedLib.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --print-full-icfg-path --reachable --find-path-to libnested.so@target_fun ./NestedLib.apk 2> /dev/null
+    dreach --print-full-icfg-path --reachable --find-path-to libnested.so@target_fun ./NestedLib.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --use-flowdroid --reachable --find-path-to libnested.so@target_fun ./NestedLib.apk 2> /dev/null
+    dreach --use-flowdroid --reachable --find-path-to libnested.so@target_fun ./NestedLib.apk 2> /dev/null
     echo ""
 }
 
@@ -102,13 +102,13 @@ function indirect_jmp {
     echo "indirect_jmp"
     echo "============"
     echo "Using Androguard:"
-    liana --reachable --find-path-to libnative.so@target_fun ./IndirectJmp.apk 2> /dev/null
+    dreach --reachable --find-path-to libnative.so@target_fun ./IndirectJmp.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./IndirectJmp.apk 2> /dev/null
+    dreach --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./IndirectJmp.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./IndirectJmp.apk 2> /dev/null
+    dreach --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./IndirectJmp.apk 2> /dev/null
     echo ""
 }
 
@@ -116,13 +116,13 @@ function indirect_jmp_variant {
     echo "indirect_jmp_variant"
     echo "===================="
     echo "Using Androguard:"
-    liana --angr-bb-iterations 2 --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant.apk 2> /dev/null
+    dreach --angr-bb-iterations 2 --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --angr-bb-iterations 2 --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant.apk 2> /dev/null
+    dreach --angr-bb-iterations 2 --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --angr-bb-iterations 2 --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant.apk 2> /dev/null
+    dreach --angr-bb-iterations 2 --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant.apk 2> /dev/null
     echo ""
 }
 
@@ -130,13 +130,13 @@ function indirect_jmp_variant2 {
     echo "indirect_jmp_variant2"
     echo "====================="
     echo "Using Androguard:"
-    liana --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant2.apk 2> /dev/null
+    dreach --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant2.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant2.apk 2> /dev/null
+    dreach --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant2.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant2.apk 2> /dev/null
+    dreach --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./IndirectJmpVariant2.apk 2> /dev/null
     echo ""
 }
 
@@ -144,13 +144,13 @@ function high_depth {
     echo "high_depth"
     echo "=========="
     echo "Using Androguard:"
-    liana --reachable --find-path-to libnative.so@target_fun ./HighDepth.apk 2> /dev/null
+    dreach --reachable --find-path-to libnative.so@target_fun ./HighDepth.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./HighDepth.apk 2> /dev/null
+    dreach --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./HighDepth.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./HighDepth.apk 2> /dev/null
+    dreach --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./HighDepth.apk 2> /dev/null
     echo ""
 }
 
@@ -158,13 +158,13 @@ function indirect_jmp_high_depth {
     echo "indirect_jmp_high_depth"
     echo "======================="
     echo "Using Androguard:"
-    liana --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepth.apk 2> /dev/null
+    dreach --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepth.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepth.apk 2> /dev/null
+    dreach --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepth.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepth.apk 2> /dev/null
+    dreach --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepth.apk 2> /dev/null
     echo ""
 }
 
@@ -172,13 +172,13 @@ function indirect_jmp_high_depth_variant {
     echo "indirect_jmp_high_depth_variant"
     echo "==============================="
     echo "Using Androguard:"
-    liana --angr-max-calldepth 10 --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepthVariant.apk 2> /dev/null
+    dreach --angr-max-calldepth 10 --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepthVariant.apk 2> /dev/null
     echo ""
     echo "Full iCFG Path:"
-    liana --angr-max-calldepth 10 --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepthVariant.apk 2> /dev/null
+    dreach --angr-max-calldepth 10 --print-full-icfg-path --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepthVariant.apk 2> /dev/null
     echo ""
     echo "Using FlowDroid:"
-    liana --angr-max-calldepth 10 --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepthVariant.apk 2> /dev/null
+    dreach --angr-max-calldepth 10 --use-flowdroid --reachable --find-path-to libnative.so@target_fun ./IndirectJmpHighDepthVariant.apk 2> /dev/null
     echo ""
 }
 
